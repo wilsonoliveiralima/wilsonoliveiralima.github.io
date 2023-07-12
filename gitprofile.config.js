4b9701d84001c993f89518fa696a2a4c448f3c82
Wilson <oliveiralima@protonmail.com>
2023-07-12 20:13:54 -0300

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,7 +2,7 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'wilssola', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
@@ -11,115 +11,109 @@ const config = {
     },
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
+    linkedin: 'wilsonoliveiralima',
+    twitter: '',
+    mastodon: '',
     facebook: '',
     instagram: '',
     youtube: '', // example: 'pewdiepie'
     dribbble: '',
-    behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    stackoverflow: '', // example: '1/jeff-atwood'
+    behance: 'wilssola',
+    medium: '',
+    dev: 'wilssola',
+    stackoverflow: '11551591/wilssola', // example: '1/jeff-atwood'
     skype: '',
     telegram: '',
-    website: 'https://arifszn.com',
+    website: 'https://tecwolf.com.br',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'oliveiralima@tecwolf.com.br',
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      '', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
+    'C#',
+    'Unity 3D',
+    'Dart',
+    'Flutter',
+    'Unreal Engine',
     'PHP',
-    'Laravel',
     'JavaScript',
+    'TypeScript',
     'React.js',
-    'Node.js',
     'Nest.js',
+    'Node.js',
     'MySQL',
     'PostgreSQL',
     'Git',
     'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
+      company: '310 Games',
+      position: 'Freelancer - Game Developer',
+      from: 'March 2019',
+      to: 'July 2019',
+      companyLink: '',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Upwork',
+      position: 'Freelancer - Software Developer',
+      from: 'January 2021',
+      to: 'November 2022',
+      companyLink: '',
     },
   ],
-  /* certifications: [
+  certifications: [
     {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com'
+      name: '',
+      body: '',
+      year: '',
+      link: ''
     },
-  ], */
+  ],
   education: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'UESC',
+      degree: 'Student BSc Physic - Not Finished',
+      from: '2020',
+      to: '2022',
     },
     {
-      institution: 'Institution Name',
+      institution: 'Student BSc Production Engineering',
       degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      from: '2023',
+      to: 'Present',
     },
   ],
 
   // To hide the `My Projects` section, keep it empty.
   externalProjects: [
     {
-      title: 'Project Name',
+      title: '',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
-    },
-    {
-      title: 'Project Name',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
+        '',
+      imageUrl: '',
+      link: '',
     },
   ],
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
     source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
+    username: 'wilssola', // to hide blog section, keep it empty
     limit: 2, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    id: 'G-5FNL4ZRVVE', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
-    id: '',
+    id: '3571279',
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'wireframe',
+    defaultTheme: 'dracula',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
@@ -133,6 +127,12 @@ const config = {
     hideAvatarRing: false,
 
     // Available themes. To remove any theme, exclude from here.
+    themes: [
+      'light',
+      'dark',
+      'dracula',
+    ],
+    /*
     themes: [
       'light',
       'dark',
@@ -165,8 +165,10 @@ const config = {
       'winter',
       'procyon',
     ],
+    */
 
     // Custom theme
+    /*
     customTheme: {
       primary: '#fc055b',
       secondary: '#219aaf',
@@ -176,6 +178,7 @@ const config = {
       '--rounded-box': '3rem',
       '--rounded-btn': '3rem',
     },
+    */
   },
 
   // Optional Footer. Supports plain text or HTML.
