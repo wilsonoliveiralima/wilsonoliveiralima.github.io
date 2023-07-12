6aa4fdc58030046a88bb95c8650ca5a58d991015
Wilson <oliveiralima@protonmail.com>
2023-07-12 20:16:34 -0300

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -182,11 +182,7 @@ const config = {
   },
 
   // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> and ❤️`,
+  footer: '',
 };
 
 export default config;
